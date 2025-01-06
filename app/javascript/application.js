f31328c68d3cbc5753ce87edfe7c84734fc31e69
Hamilton <jhamilton.oliveira@gmail.com>
2025-01-05 21:14:07 -0300

    Ativando PWA

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,15 @@ import "@hotwired/turbo-rails"
 import "@popperjs/core";
 import "bootstrap";
 import "controllers";
+
+
+if ('serviceWorker' in navigator) {
+  // Register the service worker
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+}
--- app/views/pwa/service-worker.js
+++ app/views/pwa/service-worker.js
@@ -1,26 +1,11 @@
-// Add a service worker for processing Web Push notifications:
-//
-// self.addEventListener("push", async (event) => {
-//   const { title, options } = await event.data.json()
-//   event.waitUntil(self.registration.showNotification(title, options))
-// })
-//
-// self.addEventListener("notificationclick", function(event) {
-//   event.notification.close()
-//   event.waitUntil(
-//     clients.matchAll({ type: "window" }).then((clientList) => {
-//       for (let i = 0; i < clientList.length; i++) {
-//         let client = clientList[i]
-//         let clientPath = (new URL(client.url)).pathname
-//
-//         if (clientPath == event.notification.data.path && "focus" in client) {
-//           return client.focus()
-//         }
-//       }
-//
-//       if (clients.openWindow) {
-//         return clients.openWindow(event.notification.data.path)
-//       }
-//     })
-//   )
-// })
+// app/views/pwa/service-worker.js
+
+// The install event is fired when the service worker is first installed
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installed');
+});
+
+// The activate event is fired after the install event when the service worker is actually controlling the page
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activated');
+});
